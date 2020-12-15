3d962c60e204d9f0c38ff2f14223bc60d7a52b67
Tharlys Dias <tharlys.souza@outlook.com>
2020-12-15 06:27:22 -0300

    Loop/laços

---
+++ aula7.js
@@ -0,0 +1,11 @@
+var i = 0;
+var j = 100;
+
+for (i = 0; i < j; i++) {
+    // pode ser usado no banco de dados
+    // if (pessoa == joao)
+    if (i == 10) {
+        continue;
+    }
+    console.log("O valor atual de i é: " + i);
+}
